1bd9ddc569c59ed56de0aea450a77a07b9417941
Zim97 <112665921+Zim97@users.noreply.github.com>
2023-12-04 16:55:35 -0500

    adding Unsplash API

--- main.js
+++ main.js
@@ -1,13 +1,64 @@
-const count = 10;
-const apiKey = 'Zl9REMQZF-N383BTn6-Snh1VBLy8gZ-x1BouzhMP7ro';
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count${count}`;
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let imageArray = [];
+
+const count = 30;
+const apiKey = "Zl9REMQZF-N383BTn6-Snh1VBLy8gZ-x1BouzhMP7ro";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function imageLoaded() {
+  console.log("images loaded");
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    loader.hidden = true;
+    ready = true;
+  }
+}
+
+function displayImages() {
+  imagesLoaded = 0;
+  totalImages = imageArray.length;
+  imageArray.forEach((photo) => {
+    //Create <a> to link to Unsplash
+    const item = document.createElement("a");
+    item.setAttribute("href", photo.links.html);
+    item.setAttribute("target", "_blank");
+
+    //Create <img> for image
+    const img = document.createElement("img");
+    img.setAttribute("src", photo.urls.regular);
+    img.setAttribute("alt", photo.alt_description);
+    img.setAttribute("title", photo.alt_description);
+    img.addEventListener("load", imageLoaded);
+    // Put <img> inside of <a> and then put both inside Image-container Element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
 
 async function getImages() {
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
-    } catch(error) {
-        alert("failed to fetch photos");
-    }
+  try {
+    const response = await fetch(apiUrl);
+    imageArray = await response.json();
+    displayImages();
+    // console.log(imageArray);
+  } catch (error) {
+    alert("failed to fetch photos");
+  }
 }
+
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+    getImages();
+  }
+});
+
+getImages();
